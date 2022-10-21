8e74b27d3c19c2db463c94ded773d646cb407696
steve <steve.jones763@gmail.com>
2022-10-21 19:14:56 +0100

    failing test when account ID is valid.

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -2,16 +2,32 @@ package uk.gov.dwp.uc.pairtest;
 
 import org.hamcrest.core.Is;
 import org.junit.Test;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class TicketServiceImplTest {
 
+    private final TicketServiceImpl underTest = new TicketServiceImpl();
+
+    @Test
+    public void processesRequestWhenAccountIdIsValid() {
+        SeatReservationService seatReservationService = mock(SeatReservationService.class);
+        TicketPaymentService ticketPaymentService = mock(TicketPaymentService.class);
+
+        underTest.purchaseTickets(1L, null);
+
+        verify(ticketPaymentService).makePayment(1L, 0);
+        verify(seatReservationService).reserveSeat(1L, 0);
+
+    }
     @Test
     public void shouldThrowWhenAccountIdIsNull() {
-        TicketServiceImpl underTest = new TicketServiceImpl();
         try {
             underTest.purchaseTickets(null, null);
             fail("Should throw InvalidPurchaseException when accountID is null");
@@ -22,7 +38,6 @@ public class TicketServiceImplTest {
 
     @Test
     public void shouldThrowWhenAccountIdIsZero() {
-        TicketServiceImpl underTest = new TicketServiceImpl();
         try {
             underTest.purchaseTickets(0L, null);
             fail("Should throw InvalidPurchaseException when accountID is less than 1");
@@ -33,7 +48,6 @@ public class TicketServiceImplTest {
 
     @Test
     public void shouldThrowWhenAccountIdIsNegative() {
-        TicketServiceImpl underTest = new TicketServiceImpl();
         try {
             underTest.purchaseTickets(-1L, null);
             fail("Should throw InvalidPurchaseException when accountID is less than 1");
